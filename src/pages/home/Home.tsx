2adae16104bc02d8edd24b89b141dd02943b35bc
LimaLeo13 <limaleo13@hotmail.com>
2023-10-26 14:35:55 -0300

    fix: home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,43 +1,35 @@
-import { useContext } from "react"
-import { AuthContext } from "../../contexts/AuthContext"
-
 function Home() {
-    
-    const {nome} = useContext(AuthContext)
-
 
     return (
-        <>
-            <div className="bg-indigo-900 flex justify-center">
-                <div className='container grid grid-cols-2 text-white'>
-                    <div className="flex flex-col gap-4 items-center justify-center py-4">
-                        <h2 className='text-5xl font-bold'>
-                            Seja Bem Vinde! {nome}
-                        </h2>
-                        <p className='text-xl'>
-                            Expresse aqui seus pensamentos e opniões
-                        </p>
-
-                        <div className="flex justify-around gap-4">
-                            <div className='rounded text-white 
+      <>
+        <div className="bg-indigo-900 flex justify-center">
+          <div className='container grid grid-cols-2 text-white'>
+            <div className="flex flex-col gap-4 items-center justify-center py-4">
+            <h2 className='text-5xl font-bold'>Bem Vindes!</h2>
+              <p className='text-xl'>
+                Expresse aqui seus pensamentos e opniões. Sempre com respeito!
+              </p>
+  
+              <div className="flex justify-around gap-4">
+                <div className='rounded text-white 
                                             border-white border-solid border-2 py-2 px-4'
-                                >
-                                Nova Postagem
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="flex justify-center ">
-                        <img
-                            src="https://i.imgur.com/fyfri1v.png"
-                            alt="Imagem Página Home"
-                            className='w-2/3'
-                        />
-                    </div>
+                >
+                  Nova Postagem
                 </div>
+              </div>
             </div>
-        </>
+  
+            <div className="flex justify-center ">
+              <img
+                src="https://i.imgur.com/fyfri1v.png"
+                alt="Imagem Página Home"
+                className='w-2/3'
+              />
+            </div>
+          </div>
+        </div>
+      </>
     )
-}
-
-export default Home
+  }
+  
+  export default Home
